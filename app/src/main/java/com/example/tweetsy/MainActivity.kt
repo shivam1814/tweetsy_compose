73311fe2103c4afb4a204e075edba223b49933f0
shivam <mandaliashivam72@gmail.com>
2025-03-29 23:43:25 +0530

    implement category and detail screen by shivam

--- app/src/main/java/com/example/tweetsy/MainActivity.kt
+++ app/src/main/java/com/example/tweetsy/MainActivity.kt
@@ -1,65 +1,26 @@
 package com.example.tweetsy
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tweetsy.api.TweetsyApi
+import com.example.tweetsy.screens.CategoryScreen
+import com.example.tweetsy.screens.DetailScreen
 import com.example.tweetsy.ui.theme.TweetsyTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-
-    @Inject
-    lateinit var tweetsyApi: TweetsyApi
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        GlobalScope.launch {
-            val response = tweetsyApi.getCategories()
-            Log.d("CODE", response.body()!!.distinct().toString())
-        }
-
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             TweetsyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                CategoryScreen()
+                DetailScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TweetsyTheme {
-        Greeting("Android")
     }
 }
